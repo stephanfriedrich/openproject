b4e7ec1488a4ed0599b1d9ad3bf44cc71eeec643
Marcello Rocha <marcello.rocha@gmail.com>
2024-11-27 02:17:59 -0300

    Moves nextcloud FilePathToIdMap

--- app/models/remote_identity.rb
+++ app/models/remote_identity.rb
@@ -35,4 +35,6 @@ class RemoteIdentity < ApplicationRecord
 
   validates :user, uniqueness: { scope: %i[auth_source integration] }
   validates :origin_user_id, :user, :auth_source, :integration, presence: true
+
+  scope :of_user_and_client, ->(user, client) { find_by(user:, oauth_client: client) }
 end
